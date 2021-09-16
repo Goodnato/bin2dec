151bec085d4726f86ca4f72da01f01905fdc16a1
Renato Nascimento <renatoalves_nascimento@hotmail.com>
2021-09-15 22:54:36 -0300

    Install SprintBoot

---
+++ src/java/sprintboot-bin2dec/src/main/java/com/bin2dec/sprintbootbin2dec/SprintbootBin2decApplication.java
@@ -0,0 +1,13 @@
+package com.bin2dec.sprintbootbin2dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootBin2decApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootBin2decApplication.class, args);
+	}
+
+}
---
+++ src/java/sprintboot-bin2dec/src/test/java/com/bin2dec/sprintbootbin2dec/SprintbootBin2decApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bin2dec.sprintbootbin2dec;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootBin2decApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(1, 1);
+	}
+
+}
